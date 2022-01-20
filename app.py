10bdc68c91df03a6cd8f4268d9aef598cdf938bd
CiabHatta <jacomossa@gmail.com>
2022-01-20 12:37:22 +0100

    icaimballaggi

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, jsonify, json, sess
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from flask_mail import Mail,Message
 
 options = Options()
 options.headless = True
@@ -11,6 +12,29 @@ driver.get("https://blog.rapidpack.it/blog/")
 
 app = Flask(__name__)
 app.secret_key='ffdbbbf98ijtpoijg'
+
+
+app.config.update(
+    MAIL_SERVER = "mail.arredorealistico.com" ,
+    MAIL_PORT = 465,
+    MAIL_USERNAME = "jacopo@arredorealistico.com",
+    MAIL_PASSWORD = "Ciuchino1998",
+    MAIL_USE_SSL = True,
+    MAIL_USE_TLS=False,
+    MAIL_DEFAULT_SENDER = "jacopo@arredorealistico.com",
+)
+
+mail = Mail()
+mail.init_app(app)
+
+#bot = telepot.Bot('1553708912:AAFkzmIxccZBUn3sBC7Gzv-6GtiObAKIjMw')
+
+def send_mail(to,subject,text,template, **kwargs):
+    msg = Message(subject=subject,sender='jacopo@arredorealistico.com', recipients=to)
+    msg.body = text
+    msg.html = render_template(template+'.html', **kwargs)
+    mail.send(msg)
+
 
 @app.get("/")
 def Home():
@@ -23,6 +47,24 @@ def Home():
     print(posts)
 
     return render_template("index.html", posts = posts)
+
+@app.get('/registra-anagrafica')
+def angrafica_template():
+
+    return render_template("new-customer.html")
+
+@app.post('/registra-anagrafica')
+def registerAnagrafica():
+
+    data = request.form
+
+    print(data.to_dict())
+    
+    
+
+    send_mail(["jacopo@icaimballaggi.it"],"Nuova anagrafica ICA", "Nuova angrafica ICA","email", data=data )
+
+    return jsonify(msg = "Anagrafica creata correttamente"),200
 
 if __name__ == '__main__':
     app.run(debug=True,host="localhost", port=3000)
